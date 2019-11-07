01efc3c30048aa80741c521b332b37ac719e94a0
DiffUni <57435119+DiffUni@users.noreply.github.com>
2019-11-07 10:56:38 +0100

    esercizio_triangoli-v.1.0
    programma completato risolto problema con le funzioni

--- df_compito_triangoli.c
+++ df_compito_triangoli.c
@@ -4,46 +4,75 @@
 
 int InputLatoA()
 {
-  int d;
+  int p;
   printf("dammi la lungezza del primo lato (A)\n");
-  scanf("%d\n", &d);
-  return d;
+  scanf("%d", &p);
+  return p;
 }
 
 int InputLatoB(){
   int e;
   printf("dammi la lungezza del secondo lato (B)\n");
-  scanf("%d\n", &e);
+  scanf("%d", &e);
   return e;
 }
 
 int InputLatoC(){
   int f;
-  printf("dammi la lungezza del secondo lato (C)\n");
-  scanf("%d\n", &f);
+  printf("dammi la lungezza del terzo lato (C)\n");
+  scanf("%d", &f);
   return f;
 }
 
-int main(void) {
+int main() {
 
  int a=0,b=0,c=0;
-
+do {
+ a=InputLatoA();
  b=InputLatoB();
  c=InputLatoC();
- a=InputLatoA();
+
  printf("a= %d\n", a);
  printf("b= %d\n", b);
  printf("c= %d\n", c);
-/*do {
-  do {
+
     //prendo i valori del triangolo e controllo che essi non siano <0
 
     if (a<0 || b<0 || c<0 ) {
-         printf("uno dei lati non è maggiore di 0, perfavore sceliere valori adeguati.\n", );
+         printf("uno dei lati non è maggiore di 0, perfavore sceliere valori adeguati.\n");
     }
-  } while(a<0 || b<0 || c<0);
-*/
 
+    //controllo che i valori siano minori della somma degli altri 2 e maggiori della loro differenza
+    while(a<abs(b-c)||a>b+c){
+      printf("questo 'A' non va bene!\n");
+      a=InputLatoA();
+    }
+    while(b<abs(a-c)||b>a+c){
+      printf("questo 'B' non va bene!\n");
+      a=InputLatoB();
+    }
+    while(c<abs(a-b)||c>a+b){
+      printf("questo 'B' non va bene!\n");
+      a=InputLatoC();
+    }
+
+  } while(a<0 || b<0 || c<0);
+
+//controllo che tipo di triangolo è
+
+  if (a==b && a==c) {
+    printf("equilatero\n");
+  }else if(a==b||b==c||a==c){
+    printf("isoscele\n");
+  } else{
+    printf("scaleno\n");
+  }
+
+//controllo se è rettangolo
+if(a*a==b*b+c*c || b*b==c*c+a*a || c*c==a*a+b*b)                      //senza dover riordinare controllo (attraverso piatgora) se uno dei lati è un ipotenusa.
+{
+  printf("I lati forniti sono di un triangolo rettangolo.\n");
+}
 
   return 0;
 }
